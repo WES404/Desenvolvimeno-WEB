35b520aa0b893273d94cee8d3df7fe5f2b0e6cdf
Weslei Rodrigues <69604469+WES404@users.noreply.github.com>
2021-08-30 20:08:05 -0300

    Add files via upload

--- JavaScript/APP Mata Mosquito/jogo.js
+++ JavaScript/APP Mata Mosquito/jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
@@ -13,6 +14,24 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+	//remover mosquito anterior
+
+	if (document.getElementById("mosquito")) {
+
+		document.getElementById("mosquito").remove()
+
+		if (vidas > 3) {
+			//
+		} else {
+
+			document.getElementById("v" + vidas).src = "imagens/coracao_vazio.png"
+			vidas++
+			
+		}
+
+
+	}
+
 	var x = Math.floor(Math.random() * largura) - 90
 	var y = Math.floor(Math.random() * altura) - 90
 
@@ -24,11 +43,47 @@ function posicaoRandomica() {
 	var mosquito = document.createElement('img')
 
 	mosquito.src = 'imagens/mosca.png'
-	mosquito.className = 'mosquito1'
+	mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio()
 
 	mosquito.style.left = x + "px"
 	mosquito.style.top = y + "px"
 	mosquito.style.position = 'absolute'
 
+	mosquito.id = 'mosquito'
+
+	mosquito.onclick = function() {
+		this.remove()
+	}
+
 	document.body.appendChild(mosquito)
+
+
+}
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+			
+		case 1:
+			return 'mosquito2'			
+
+		case 2:
+			return 'mosquito3'
+	}
+}
+
+function ladoAleatorio() {
+	var lado = Math.floor(Math.random() * 2)
+
+	switch(lado) {
+		case 0:
+			return 'ladoA'
+			
+		case 1:
+			return 'ladoB'
+	}
+
 }
