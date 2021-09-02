42af3097b2c938457e4e7069604950c791b54a1b
Weslei Rodrigues <69604469+WES404@users.noreply.github.com>
2021-09-02 18:02:59 -0300

    Add files via upload

--- JavaScript/APP Mata Mosquito/jogo.js
+++ JavaScript/APP Mata Mosquito/jogo.js
@@ -2,15 +2,51 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 10
+
+var nivel = window.location.search.replace("?", "")
+
+var criaMosquitoTempo
+
+if(nivel === "normal") {
+
+	criaMosquitoTempo = 1500
+
+} else if(nivel === "dificil") {
+
+	criaMosquitoTempo = 1000
+
+} else if(nivel === "chucknorris") {
+
+	criaMosquitoTempo = 750
+
+}
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
 	largura = window.innerWidth
 
-	//console.log(altura, largura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+
+	tempo -= 1
+
+	if(tempo < 0) {
+		// Se tempo der 0 o jogo é vencido. Não há necessidade de checar os corações
+
+		clearInterval(cronometro)
+		clearInterval(Criamosquito)
+		window.location.href = "vitoria.html"
+
+	}else{
+
+	document.getElementById("cronometro").innerHTML = tempo
+
+	}
+}, 1000)
 
 function posicaoRandomica() {
 
@@ -21,7 +57,9 @@ function posicaoRandomica() {
 		document.getElementById("mosquito").remove()
 
 		if (vidas > 3) {
-			//
+			
+			window.location.href = "fim_de_jogo.html"
+
 		} else {
 
 			document.getElementById("v" + vidas).src = "imagens/coracao_vazio.png"
